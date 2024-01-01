10f864f0eb9853eaec424603c260d772d6ce4738
Gustavo Vargas <ge.vargasn@gmail.com>
2024-01-01 14:08:12 +0100

    changes for lambda

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import requests
 
 # Función para llamar a la función Lambda de AWS
 def call_lambda_function(input_text):
-    lambda_endpoint = "https://your-api-gateway-url"
-    # response = requests.post(lambda_endpoint, json={"text": input_text})
-    # response = response.json()
-    response = {"text": "texto de respuesta"}
+    lambda_endpoint = st.secrets["LAMBDA_URL"]
+    response = requests.post(lambda_endpoint, json={"input_string": input_text})
+    print("\nResponse from Lambda function:", response)
+    response = response.json()
     return response
+
 
 # Interfaz de usuario de Streamlit
 st.title("CV Search App")
@@ -18,5 +19,5 @@ user_input = st.text_input("Search for:", "")
 if user_input:
     st.write("Results:")
     results = call_lambda_function(user_input)
-
-    st.write(results["text"])
+    print("\nResults:", results)
+    st.write(results)
